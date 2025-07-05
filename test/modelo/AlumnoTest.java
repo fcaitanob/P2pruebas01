4e0581b7a2782b0d1f1b7cc0000169b75402ac6a
fernando.caitano <fernando.caitano@RM377.RMF.local>
2025-07-05 07:35:13 -0300

    prueba_unitarias: testAgregarAdministrador y ExternoTest

--- test/modelo/AlumnoTest.java
+++ test/modelo/AlumnoTest.java
@@ -24,4 +24,14 @@ class AlumnoTest {
 		assertTrue(texto.contains("Alumno uno"));
 	}
 
+	@Test
+	void testAgregarAdministrador() {
+		AlumnoAux aluAux = new AlumnoAux(111, "Alumno uno");
+		Administrador admAux = new Administrador(111111);
+		aluAux.agregarAdministrador(admAux);		
+		String texto = aluAux.toString();
+        assertEquals(texto, "Persona [CI=111]\n"
+        		+ "Alumno [nombre=Alumno uno]\n"
+        		+ "SecAdministradores cant: 1\n");
+	}
 }
